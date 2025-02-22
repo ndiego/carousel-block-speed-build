d056e7fc073bcaf5e4f37d32b194fb2004f50b12
Nick Diego <nickmdiego@gmail.com>
2025-02-22 09:02:15 +0800

    Fresh build.

--- build/carousel.asset.php
+++ build/carousel.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => 'c4e7254f2babcb22d34b');
+<?php return array('dependencies' => array('wp-element'), 'version' => 'e155bc7d263d8aac6873');
